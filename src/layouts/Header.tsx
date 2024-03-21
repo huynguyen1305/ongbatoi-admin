74dacccd859fdcd80eaadab86cb16214785da685
huynguyen1305 <huynguyen1305.dev@gmail.com>
2024-03-22 01:28:04 +0700

    update src

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,16 +1,34 @@
 import { Layout, Typography } from "antd";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const { Header } = Layout;
 
 const HeaderComponent = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const user = localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user") || "{}")
+    : null;
 
   return (
-    <Header className="flex flex-col justify-center h-16 px-10">
+    <Header className="flex items-center justify-between h-16 px-10">
       <Typography className="text-2xl font-semibold capitalize">
         {pathname.substring(1).replace(/[\/]/g, " > ").replace(/[\/-]/g, " ")}
       </Typography>
+      <div className="flex gap-2">
+        <Typography className="text-2xl font-semibold">
+          {user?.username},
+        </Typography>
+        <Typography
+          className="text-2xl font-semibold capitalize cursor-pointer"
+          onClick={() => {
+            localStorage.clear();
+            navigate("/login");
+          }}
+        >
+          Log out
+        </Typography>
+      </div>
     </Header>
   );
 };
